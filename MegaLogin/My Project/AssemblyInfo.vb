5c5a313f43316b708bd59a29eb9805282b686bdd
Aljon Sumilhig <33595791+assumilhig@users.noreply.github.com>
2025-03-19 16:46:58 +0800

    update commands

--- MegaLogin/My Project/AssemblyInfo.vb
+++ MegaLogin/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.8.31.2022")> 
+<Assembly: AssemblyVersion("1.25.3.19")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
--- MegaLogin/modHelpers.vb
+++ MegaLogin/modHelpers.vb
@@ -29,15 +29,27 @@ Module modHelpers
         Return sOutput
     End Function
 
-    Public Function CheckSession()
-        Return CommandShell("/c cd " & MEGAcmd & " & " & MEGAcmd & "\mega-session.bat")
+    Private Function ExecuteCommand(ByVal command As String) As String
+        Console.WriteLine("Executing: " & command)
+        Return CommandShell(command)
     End Function
 
-    Public Function LoginMegaAccount(ByVal Username As String, ByVal Password As String)
-        Return CommandShell("/c cd " & MEGAcmd & " & " & MEGAcmd & "\mega-login.bat " & Username & " " & Password)
+    Public Function CheckSession() As String
+        Dim scriptPath As String = Path.Combine(MEGAcmd, "mega-session.bat")
+        Dim command As String = String.Format("cd ""{0}"" & ""{1}""", MEGAcmd, scriptPath)
+        Return ExecuteCommand(command)
     End Function
 
-    Public Function LogoutMegaAccount()
-        Return CommandShell("/c cd " & MEGAcmd & " & " & MEGAcmd & "\mega-logout.bat")
+    Public Function LoginMegaAccount(ByVal Username As String, ByVal Password As String) As String
+        Dim scriptPath As String = Path.Combine(MEGAcmd, "mega-login.bat")
+
+        Dim command As String = String.Format("cd ""{0}"" & ""{1}"" ""{2}"" ""{3}""", MEGAcmd, scriptPath, Username, Password)
+        Return ExecuteCommand(command)
+    End Function
+
+    Public Function LogoutMegaAccount() As String
+        Dim scriptPath As String = Path.Combine(MEGAcmd, "mega-logout.bat")
+        Dim command As String = String.Format("cd ""{0}"" & ""{1}""", MEGAcmd, scriptPath)
+        Return ExecuteCommand(command)
     End Function
 End Module
